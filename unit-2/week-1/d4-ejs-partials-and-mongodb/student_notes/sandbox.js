72a0b71490f04fc48bab71b99b4416b124274f87
anthonykhong <anthonykhong95200@gmail.com>
2023-03-16 17:59:43 +0100

    update

---
+++ unit-2/week-1/d4-ejs-partials-and-mongodb/student_notes/sandbox.js
@@ -0,0 +1,76 @@
+// console.log("Good morning");
+
+// recursion.js
+// function dream() {
+//   return "wake up";
+//   dream();
+// }
+// console.log(dream());
+
+// function countUp(num) {
+//   for (let i = 1; i <= num.length; i++) {
+//     return console.log(i);
+//   }
+// }
+
+// console.log(countUp(7));
+
+// function countDown(num) {
+//   for (let i = 7; i >= num.length; i++) {
+//     console.log(i);
+//   }
+// }
+
+// console.log(countDown(7));
+
+// function recursiveCountdown(startingNum) {
+//   // Base case/ Terminating condition
+//   if (startingNum === -5) return "We have woken up";
+//   console.log(startingNum);
+
+//   recursiveCountdown(startingNum - 1);
+// }
+
+// console.log(recursiveCountdown(7));
+
+// function calculateTotal(num) {
+//   let sum = 0;
+//   for (let i = 0; i <= num; i++) {
+//     sum += i;
+//   }
+//   return sum;
+// }
+
+// console.log(calculateTotal(5));
+
+// Recursive approach to calculate the SUM
+// let total = 0;
+
+// function calculateSumRecursive(num) {
+//   // Base case/ Terminating condition
+//   if (num === 0) return 0;
+//   return num + calculateSumRecursive(num - 1);
+// }
+
+// console.log(calculateSumRecursive(5));
+
+// function factorial(num) {
+//   // base case/ Terminating condition
+//   if (num === 1) return 1;
+//   return num * factorial(num - 1);
+// }
+
+// console.log(factorial(3));
+
+const teamStructure = {
+    name: "Bill gates",
+    teams: [],
+},
+{
+    name: "",
+    teams: [],
+},
+{
+    name: "",
+    teams: [],
+},
