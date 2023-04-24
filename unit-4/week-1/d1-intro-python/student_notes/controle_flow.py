9e382e00230a72f5120cc38ebb843b9a00b926bc
anthonykhong <anthonykhong95200@gmail.com>
2023-04-24 23:38:00 +0200

    .

---
+++ unit-4/week-1/d1-intro-python/student_notes/controle_flow.py
@@ -0,0 +1,43 @@
+# floor = "sticky"
+# walls ="clean"
+# if floor == "sticky":
+#     print("Clean the floor! It's sticky")
+# if walls == "sticky":
+#     print("Clean the walls. They're stciky!")
+
+# color = input('Enter "green", "yellow", "red": ').lower()
+# print(f'The user entered {color}')
+
+# if color == "green":
+#     print("Go!")
+# elif color == "yellow":
+#     print("Slow Down!")
+# elif color == "red":
+#     print("Stop!")
+# else:
+#     print("Bogus!")
+
+# names = ["Tom", "Deborah", "Murray", "Axel"]
+
+# for name in names:
+#   if name == "Murray":
+#     continue
+#   print(name)
+
+# color = input('Enter "green", "yellow", "red": ').lower()
+# print(f'The user entered {color}')
+
+# while color != "quit":
+#     if color == "green":
+#         print("Go!")
+#     elif color == "yellow":
+#         print("Slow Down!")
+#     elif color == "red":
+#         print("Stop!")
+#     else:
+#         print("Bogus!")
+#     color = input('Enter "green", "yellow", "red": ').lower()
+#     print(f'The user entered {color}')
+
+for odd in range(1, 20, 2):
+    print(odd)
