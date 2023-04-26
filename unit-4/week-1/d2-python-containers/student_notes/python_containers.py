7e724eba3932f08069a72f0401f352d17088330f
anthonykhong <anthonykhong95200@gmail.com>
2023-04-26 18:59:20 +0200

    .

---
+++ unit-4/week-1/d2-python-containers/student_notes/python_containers.py
@@ -0,0 +1,68 @@
+'''
+DICTIONARIES
+'''
+
+# student = {
+#   'name': 'Maria T',
+#   'course': 'SEI BTY',
+#   'current_week': 10
+# } 
+
+# del student["course"]
+
+# if 'course' in student:
+#   print( f"{student['name']} is enrolled in {student['course']}")
+# else:
+#   print( f"{student['name']} is not enrolled in a course")
+
+# student["graduated"] = "yes"
+
+# --NOT THE BEST WAY--
+# for key in student:
+#   print( f"{key} = {student[key]}" )
+
+# --A BETTER WAY--
+# for student_item in student.items():
+#   print( student_item)
+
+# --BEST WAY--
+# for key, val in student.items():
+#   print( f"{key} = {val}" )
+
+# where_my_things_are = {
+#     "laptop": "on my desk",
+#     "phone": "in my pocket",
+#     "will to live": "not found"
+# }
+
+# for key, val in where_my_things_are.items():
+#     print(f"My {key} is kept {val}")
+
+'''
+LISTS
+'''
+
+# colors = ['red', 'green', 'blue']
+# colors.append("purple")
+# colors.extend(["yellow", "brown"])
+# colors.insert(2, "greenish-blue")
+
+# odds = [1, 3, 5]
+# evens = [2, 4, 6]
+# nums = odds + evens
+# print(nums)
+
+# scores = [
+#   {
+#     'name': 'Darrell',
+#     'points': 25  # points the player scored
+#   }
+# ]
+
+# new_score = {'name': 'Darrell2', 'points': 20}
+# scores.append(new_score)
+
+# for score in scores:
+
+
+
