de194f7d270d7a5b2200b6db0b4a3b6ae2114381
anthonykhong <anthonykhong95200@gmail.com>
2023-04-28 18:41:28 +0200

    .

--- unit-4/week-1/d3-python-functions-and-classes/student_notes/python_classes.py
+++ unit-4/week-1/d3-python-functions-and-classes/student_notes/python_classes.py
@@ -1,1 +1,88 @@
+# nums = [1, 2, 3]
 
+# print(dir(nums))
+
+# class Dog():
+#   def __init__(self, name, age=0):
+#     self.name = name
+#     self.age = age
+
+#   def bark(self):
+#     print(f'{self.name} says woof!')
+    
+#   def __str__(self):
+#     return f'Dog named {self.name} is {self.age} years old'
+  
+# spot = Dog('Spot', 8)
+
+# print(spot) # -> Dog named Spot is 8 years old
+
+# class Vehicule:
+#     def __init__(self, make, model):
+#         self.make = make
+#         self.model = model
+#         self.running = False
+
+#     def start(self):
+#         self.running = True
+#         print("running...")
+
+#     def stop(self):
+#         self.running = False
+#         print("stopped...")
+
+#     def __str__(self):
+#         return (f"Vehicule is a {self.make} model {self.model}")
+    
+# car = Vehicule("Audi", "R8")
+
+class Dog():
+  # class attribute
+  next_id = 1
+
+  # updated __init__
+  def __init__(self, name, age=0):
+    self.name = name
+    self.age = age
+    self.id = Dog.next_id
+    Dog.next_id += 1
+
+  def bark(self):
+    print(f'{self.name} says woof!')
+
+  # updated __str__
+  def __str__(self):
+    return f'Dog ({self.id}) named {self.name} is {self.age} years old'
+  
+#   new code below
+  @classmethod
+  def get_total_dogs(cls):
+    # cls represents the Dog class
+    return cls.next_id - 1
+
+spot = Dog('Spot', 8)
+pup = Dog('Lassie')
+
+# class methods are called on the class, not an instance
+print(Dog.get_total_dogs())  # -> 2
+
+# Pass in superclass as argument
+class ShowDog(Dog):
+  # Add additional parameters AFTER those in the superclass
+  def __init__(self, name, age=0, total_earnings=0):
+    # Always call the superclass's __init__ first
+    Dog.__init__(self, name, age)
+    # Now add any new attributes
+    self.total_earnings = total_earnings
+  
+  # Add additional methods
+  def add_prize_money(self, amount):
+    self.total_earnings += amount
+    print(f'{self.name}\'s new total earnings are ${self.total_earnings}')
+
+winky = ShowDog('Winky', 3, 1000)
+print(winky) # Yay, inherited the overriden __str__
+winky.bark() # Yay, inherited the bark method
+print(winky.total_earnings) # -> 1000
+winky.add_prize_money(500) # New method that 'Dogs' don't have
+print(winky.total_earnings) # -> 1500
--- unit-4/week-1/d3-python-functions-and-classes/student_notes/python_functions.py
+++ unit-4/week-1/d3-python-functions-and-classes/student_notes/python_functions.py
@@ -1,1 +1,36 @@
-print('sssssss')
+# def fahr_to_kelvin(temp):
+#     return((temp-32)*(5/9))+273.15
+
+# function someFunction(){
+
+# }
+
+# def first_function():
+#     pass
+
+# result = first_function()
+# print(result)
+
+# def fn(*args):
+#   print( type(args) )
+#   for arg in args:
+#     print(arg)
+
+# fn(1, 2, 'SEI')
+
+# def dev_skills(dev_name, **kwargs):
+#   # kwargs will be a dictionary!
+#   dev = {'name': dev_name, 'skills': kwargs}
+#   return dev
+
+# print(dev_skills('Jackie', HTML=5, CSS=3, JavaScript=4, Python=2))
+
+def get_category(points):
+  if points > 900:
+    return 'Gold Member'
+  elif points > 800:
+    return 'Silver Member'
+  else:
+    return 'Bronze Member'
+
+print( get_category(850) )
